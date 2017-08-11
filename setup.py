8be71058faff99a49da0a2da9cfd0e1870a1da1f
Andy <andy@barnpengar.se>
2017-08-11 10:43:49 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 
 setup(
     name="ecs-deployer",
-    version="0.0.1",
+    version="0.0.2",
     packages=["ecs_deployer"],
     author="Andy Sun",
     author_email="andy_sun_sha@hotmail.com",
+    license='MIT',
+    url='https://github.com/AndySun25/ecs-deployer',
     description="Simple Python ECS deployment script.",
     scripts=["ecs_deployer/bin/ecs-deploy.py"],
     install_requires=[
