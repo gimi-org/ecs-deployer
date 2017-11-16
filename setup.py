5c7bf7bccc2421cfb034344d4c5ce3904e54ac6e
Andy <andy@barnpengar.se>
2017-11-16 14:51:09 +0100

    Overhauled config format and more verbose messages when running.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name="ecs-deployer",
-    version="0.0.2",
+    version="0.1.0",
     packages=["ecs_deployer"],
     author="Andy Sun",
     author_email="andy_sun_sha@hotmail.com",
     license='MIT',
     url='https://github.com/AndySun25/ecs-deployer',
     description="Simple Python ECS deployment script.",
-    scripts=["ecs_deployer/bin/ecs-deploy.py"],
+    scripts=["ecs_deployer/bin/ecs-deployer.py"],
     install_requires=[
         'awscli>=1.11.106'
     ],
