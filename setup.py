c0eada3f3c5cece404869cbb2bba29b84517e212
Andy <andy_sun_sha@hotmail.com>
2018-08-15 13:02:59 +0200

    Changed requirement types.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="ecs-deployer",
-    version="0.1.6",
+    version="0.1.7",
     packages=["ecs_deployer"],
     author="Andy Sun",
     author_email="andy_sun_sha@hotmail.com",
@@ -13,8 +13,8 @@ setup(
     scripts=["ecs_deployer/bin/ecs-deployer.py"],
     install_requires=[
         'boto3>=1.4.6',
-        'awscli==1.11.106',
-        'requests==2.18.4',
+        'awscli>=1.11.106',
+        'requests>=2.18.4',
     ],
     python_requires='>3.5',
 )
